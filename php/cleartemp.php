0e2fc462b441545dd328528fa2e13861bf61c5bc
dandyling <dandyling@gmail.com>
2014-01-13 20:08:55 +0900

    testing

--- php/cleartemp.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-$files = glob('../users/'.$_SESSION['pid'].'/player.png'); // get all file names
-foreach($files as $file){ // iterate files
-  if(is_file($file))
-    unlink($file); // delete file
-}
-?>
--- php/rename.php
+++
@@ -1,5 +0,0 @@
-<?php
-if(isset($_POST["oldName"]) && !empty($_POST["oldName"])){
-	rename($_POST["oldName"], $_POST["newName"]);
-}
-?>
